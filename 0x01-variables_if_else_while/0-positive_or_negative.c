3d967f801c9c66f2782cf5a08929e81e9f3a5815
32bit-dev <izuchukwuemenike17@gmail.com>
2022-09-12 16:24:39 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,27 +17,15 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
   {
-      
-      printf("%i is positive\n", n);
-      
-    }
-  
-  else if (n == 0)
-    
-    {
-      
-      printf("%i is zero\n", n);
-      
-    }
-  
-  else if (n < 0)
-    
-    {
-      
-      printf("%i is negative\n", n);
-      
-    }
-  
-  return (0);
-  
+    printf("%i is positive\n", n);
+  }
+else if (n == 0)
+  {
+    printf("%i is zero\n", n);
+  }
+else if (n < 0)
+  {
+    printf("%i is negative\n", n);
+  }
+return (0);
 }
