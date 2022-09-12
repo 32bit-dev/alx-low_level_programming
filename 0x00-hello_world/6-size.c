0d5b620ab9b2cf3e5cd1c374737429b123a1561f
32bit-dev <izuchukwuemenike17@gmail.com>
2022-09-12 11:58:23 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print the size of various type of computer
+ * Description: using the main function
+ * This programme prints the sizes of various types of computer it is run on
+ * Return: 0
+*/
+int main(void)
+{
+char c;
+int i;
+long li;
+long long lli;
+float f;
+printf("size of a char: %Id byte(s)\n", sizeof(c));
+printf("size of an int: %Id bytes(s)\n", sizeof(i));
+printf("size of a long long: %Id bytes(s)\n", sizeof(lli));
+printf("size of a float: %Id byte(s)\n", sizeof(f));
+return (0);
+}
